05ed97d0909d801be06043244432e4a166bcba2f
Jason Peak <jpeak5@lsu.edu>
2018-12-18 13:39:41 -0600

    report changes, made live in meeting with folks

--- report.php
+++ report.php
@@ -46,7 +46,7 @@ function trim_query_param($query_param) {
 }
 
 function validate_query_param($query_param) {
-  foreach (explode('-', $query_param) as $chunk) { 
+  foreach (explode('-', $query_param) as $chunk) {
     if (!ctype_alnum($chunk)) {
       throw new Exception("expected alpha-numeric (optionally hyphen-delimited) argument, got '$query_param'.  ");
     }
@@ -55,22 +55,18 @@ function validate_query_param($query_param) {
 }
 
 function get_query($where_clause) {
-    
+
     $query = <<<EOQ
 
 select
+         acc.identifier "Aspace Identifier",
          acc.title "Collection Title",
          ud.string_2 "Location",
          d.`expression` "Collection Dates",
-         (select value from enumeration_value where id=d.date_type_id) "Date Type",
-         (select value from enumeration_value where id=d.label_id) "Date Label ID", 
-
-         bulk.`expression` "Bulk Dates",
-         (select value from enumeration_value where id=bulk.date_type_id) "Bulk date type",
-         (select value from enumeration_value where id=bulk.label_id) "Bulk dat Label ID", 
 
          CONCAT(e.number, ' ', (select value from enumeration_value where id = e.extent_type_id)) Extent,
          ud.date_1 "Date Received",
+         acc.accession_date "Accession Date",
          ( SELECT value FROM enumeration_value where id = acc.acquisition_type_id) "Acquisition Type",
          acc.restrictions_apply "Restrictions Apply",
          acc.access_restrictions "Access Restrictions",
@@ -80,12 +76,12 @@ select
          acc.content_description "Content Description",
          acc.general_note "General Note",
          ud.string_1 "Mss Number",
-         people.sort_name Name,
+         people.sort_name "Source Name",
+         people.role Role,
          people.address_1 Address,
          people.city City,
          people.region Region,
          people.post_code 'Post Code',
-         acc.accession_date "Accession Date",
          ud.real_1 "Price",
          concat('https://aspace.lib.lsu.edu/accessions/', acc.id) "ArchivesSpace URL"
 
@@ -100,48 +96,54 @@ select
          (select value from enumeration_value where id = e.portion_id)  Portion,
          people.role Role,
          d.`begin` "Begin date",
-         d.`end` "End date", 
+         d.`end` "End date",
 
          bulk.`begin` "Bulk begin",
-         bulk.`end` "Bulk end", 
+         bulk.`end` "Bulk end",
 
          acc.repo_id "Aspace repo id",
          acc.id "Aspace accession id",
          acc.identifier "Aspace Identifier",
-         @num_elements := LENGTH(acc.identifier) - LENGTH(REPLACE(acc.identifier, '"', '')) AS num_elements,        
+         @num_elements := LENGTH(acc.identifier) - LENGTH(REPLACE(acc.identifier, '"', '')) AS num_elements,
          SUBSTRING_INDEX(SUBSTRING_INDEX(acc.identifier, '"', 2), '"', -1) AS id1,
          SUBSTRING_INDEX(SUBSTRING_INDEX(acc.identifier, '"', 4), '"', -1) AS id2,
            IF(@num_elements > 4, SUBSTRING_INDEX(SUBSTRING_INDEX(acc.identifier, '"', 6), '"', -1), '') AS id3,
            IF(@num_elements > 6, SUBSTRING_INDEX(SUBSTRING_INDEX(acc.identifier, '"', 8), '"', -1), '') AS id4
+         (select value from enumeration_value where id=d.date_type_id) "Date Type",
+         (select value from enumeration_value where id=d.label_id) "Date Label ID",
+
+         bulk.`expression` "Bulk Dates",
+         (select value from enumeration_value where id=bulk.date_type_id) "Bulk date type",
+         (select value from enumeration_value where id=bulk.label_id) "Bulk dat Label ID",
 
 */
 
-from 
+from
    accession acc inner join user_defined ud on ud.accession_id = acc.id
    join extent e on e.accession_id = acc.id
    join linked_agents_rlshp lar on lar.accession_id = acc.id
    join name_person np on lar.agent_person_id = np.id
    join agent_contact ac on lar.agent_person_id = ac.agent_person_id
    join date d on d.accession_id = acc.id
-   left join (select * from date d 
+   left join (select * from date d
     where (select value from enumeration_value where id=d.date_type_id) = "bulk") bulk on bulk.accession_id = acc.id
-   join (select larlship.accession_id, (select value from enumeration_value where id=larlship.role_id) role, np.sort_name, address_1, city, region, post_code 
-      from agent_contact ac 
+   join (select larlship.accession_id, (select value from enumeration_value where id=larlship.role_id) role, np.sort_name, address_1, city, region, post_code
+      from agent_contact ac
          join name_person np on ac.agent_person_id = np.agent_person_id
           join linked_agents_rlshp larlship on larlship.agent_person_id = ac.agent_person_id
           where larlship.role_id in(880,881)
       union
-      select larlship.accession_id, (select value from enumeration_value where id=larlship.role_id) role, nce.sort_name, address_1, city, region, post_code 
-      from agent_contact ac 
+      select larlship.accession_id, (select value from enumeration_value where id=larlship.role_id) role, nce.sort_name, address_1, city, region, post_code
+      from agent_contact ac
          join name_corporate_entity nce on ac.agent_corporate_entity_id = nce.agent_corporate_entity_id
           join linked_agents_rlshp larlship on larlship.agent_corporate_entity_id = ac.agent_corporate_entity_id
           where larlship.role_id in(880,881)
       union
-      select larlship.accession_id, (select value from enumeration_value where id=larlship.role_id) role, nf.sort_name, address_1, city, region, post_code 
-      from agent_contact ac 
+      select larlship.accession_id, (select value from enumeration_value where id=larlship.role_id) role, nf.sort_name, address_1, city, region, post_code
+      from agent_contact ac
          join name_family nf on ac.agent_family_id = nf.agent_family_id
           join linked_agents_rlshp larlship on larlship.agent_family_id = ac.agent_family_id
-          where larlship.role_id in(880,881)) people 
+          where larlship.role_id in(880,881)) people
       ON people.accession_id = acc.id
 where ((select value from enumeration_value where id=d.date_type_id) = "inclusive"
     or (select value from enumeration_value where id=d.date_type_id) = "single")
